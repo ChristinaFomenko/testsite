5961c7308a2b76697fc899139053a8d67f029e27
Christina <kris.inferno@gmail.com>
2021-05-06 17:27:22 +0300

    add

--- mysite/news/models.py
+++ mysite/news/models.py
@@ -1,7 +1,7 @@
 from django.db import models
+from django.urls import reverse
 
 
-# Create your models here.
 class News(models.Model):
     title = models.CharField(max_length=150, verbose_name='Наименование')
     content = models.TextField(blank=True, verbose_name='Контент')
@@ -9,11 +9,11 @@ class News(models.Model):
     updated_at = models.DateTimeField(auto_now=True, verbose_name='Обновлено')
     photo = models.ImageField(upload_to='photos/%Y/%m/$d/', verbose_name='Фото', blank=True)
     is_published = models.BooleanField(default=True, verbose_name='Опубликовано')
-    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, blank=True)
+    category = models.ForeignKey('Category', on_delete=models.PROTECT, null=True, verbose_name='Категория')
 
-    def my_func(self):
-        return 'Hello from model'
 
+    def get_absolute_url(self):
+        return reverse('view_news', kwargs={"news_id": self.pk})
 
     def __str__(self):
         return self.title
@@ -26,6 +26,10 @@ class News(models.Model):
 
 class Category(models.Model):
     title = models.CharField(max_length=150, db_index=True, verbose_name='Наименование категории')
+
+    def get_absolute_url(self):
+        return reverse('category', kwargs={"category_id": self.pk})
+
 
     def __str__(self):
         return self.title
