6a4983b14f684470699ccf8f521e6876a464eaf9
Christina <kris.inferno@gmail.com>
2021-05-13 15:47:54 +0300

    class

--- mysite/news/forms.py
+++ mysite/news/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 from .models import News
+import re
+from django.core.exceptions import  ValidationError
 
 
 class NewsForm(forms.ModelForm):
@@ -12,6 +14,12 @@ class NewsForm(forms.ModelForm):
             'content': forms.Textarea(attrs={'class': 'form-control', 'rows': 5}),
             'category': forms.Select(attrs={'class': 'form-control'}),
         }
+
+    def clean_title(self):
+        title = self.cleaned_data['title']
+        if re.match(r'\d', title):
+            raise ValidationError('Название не должно начинаться с цифры')
+        return title
     # title = forms.CharField(max_length=150, label='Название', widget=forms.TextInput(attrs={"class": "form-control"}))
     # content = forms.CharField(label='Текст', required=False, widget=forms.Textarea(attrs={
     #     "class": "form-control",
