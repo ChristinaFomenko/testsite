790319636130bdce63e248f70566189e85085177
Christina <kris.inferno@gmail.com>
2021-05-18 17:12:32 +0300

    mixin

--- mysite/news/admin.py
+++ mysite/news/admin.py
@@ -12,13 +12,19 @@ class NewsAdmin(admin.ModelAdmin):
     search_fields = ('title', 'content')
     list_editable = ('is_published',)
     list_filter = ('is_published', 'category')
-    # fields =
+    fields = ('title', 'category', 'content', 'photo', 'get_photo', 'is_published', 'views', 'created_at', 'updated_at')
+    readonly_fields = ('get_photo', 'views', 'created_at', 'updated_at')
+    save_on_top = True
 
     def get_photo(self, obj):
         if obj.photo:
             return mark_safe(f'<img src="{obj.photo.url}" width="75">')
         else:
-            return 'Фото не установлено'
+            return '-'
+
+    get_photo.short_description = 'Миниатюра'
+
+
 class CategoryAdmin(admin.ModelAdmin):
     list_display = ('id', 'title')
     list_display_links = ('id', 'title')
